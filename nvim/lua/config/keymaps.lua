fc6976de9fd605f718c199023617eadbceffa21c
aditya <adityavish126@gmail.com>
2025-01-18 01:07:23 +0530

    removed example

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,13 +2,10 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Remap Tab to Escape in insert mode
+-- Swap Escape and Tab in all modes
+vim.keymap.set({ "n", "v", "i" }, "<Esc>", "<Tab>") -- Press Esc to act like Tab
+vim.keymap.set({ "n", "v", "i" }, "<Tab>", "<Esc>") -- Press Tab to act like Esc
 
--- ~/.config/nvim/init.lua or a relevant configuration file
--- local map = vim.api.nvim_set_keymap
--- local options = { noremap = true, silent = true }
---
--- -- Keybindings for Hop.nvim
--- map('n', 's', ":HopChar2<CR>", options)
--- map('n', 'S', ":HopWord<CR>", options)
-
--- window key re change 
+-- Optional: If you want to remap Escape back to Tab in normal mode
+-- vim.keymap.set("n", "<Esc>", "<Tab>", { noremap = true, silent = true })
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,30 +1,3 @@
--- return {
-  -- add dracula
---   { "Mofiqul/dracula.nvim" ,
---     opts = {
---       transparent_bg = true,
---       styles = {
---         sidebars = true,
---         floats = true
---       }
---     }
---   },
---
---   -- Configure LazyVim to load dracula
---   {
---     "LazyVim/LazyVim",
---     opts = {
---       colorscheme = "dracula",
---       transparent_bg = true,
---       styles = {
---         sidebars = "transparent",
---         floats = "transparent",
---       },
---     },
---   }
--- }
-
---tokyonight
 return {
   {
     "folke/tokyonight.nvim",
@@ -37,19 +10,3 @@ return {
     },
   },
 }
-
--- return {
--- -- catppuccin
---   {
---     "catppuccin",
---     opts = {
---       transparent_background = true,
---     },
---   },
---   {
---     "LazyVim/LazyVim",
---     opts = {
---       colorscheme = "catppuccin",
---     },
---   },
--- }
